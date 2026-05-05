5a41749ba18dfb0485ec579515204bf07334c117
Ignacio Leitao <ignacioleitao@Ignacios-MacBook-Pro.local>
2026-05-05 16:32:06 -0300

    feat: prepare for production deployment

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,26 @@
 import Fastify from "fastify";
 
-import { authRoutes } from './routes/auth';
+import { authRoutes } from "./routes/auth";
 import { summaryRoutes } from "./routes/summary";
 import { transactionRoutes } from "./routes/transactions";
 
-const app = Fastify({ 
-  logger: true 
+const app = Fastify({
+  logger: true,
 });
 
 app.register(authRoutes);
 app.register(summaryRoutes);
 app.register(transactionRoutes);
 
-app.listen({ port: 3000 }, (err) => {
-  if (err) {
-    app.log.error(err);
-    process.exit(1);
-  }
-});
+app.listen(
+  {
+    port: Number(process.env.PORT) || 3000,
+    host: "0.0.0.0",
+  },
+  (err) => {
+    if (err) {
+      app.log.error(err);
+      process.exit(1);
+    }
+  },
+);
